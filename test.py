1608d27034f9f77c4dbcc0dfec9ca1cc4ae32163
Roberto-Sosa-CPA <robertoignacio747@gmail.com>
2022-05-10 23:02:59 -0300

    Add tweet voting system

--- test.py
+++ test.py
@@ -1,25 +1,38 @@
+from turtle import onclick
 import streamlit as st
 import pandas as pd
+import numpy as np
 import random
 
 st.title('Proyecto de clasificación de tweets violentos')
 
-
 df = pd.read_csv('preds.tsv', sep='\t')
+#votes = pd.DataFrame(index=[0], columns=['tweet','vote'])
+votes = pd.read_csv('votes.csv', sep ='\t')
+
+current_tweet = df['text'].iloc[random.randint(0,len(df))]
+
+def vote_violent_callback():
+  votes.loc[votes.shape[0]] = [current_tweet,1]
+  votes.to_csv('votes.csv',index=False,sep='\t')
+  return None
+
+def vote_non_violent_callback():
+  votes.loc[votes.shape[0]] = [current_tweet,0]
+  votes.to_csv('votes.csv',index=False,sep='\t')
+  return None
+
+def vote_mysoginist_callback():
+  votes.loc[votes.shape[0]] = [current_tweet,2]
+  votes.to_csv('votes.csv',index=False,sep='\t')
+  return None
 
 
-with st.container():
-    st.subheader('Tweet de muestra',)
-    st.write(df['text'].iloc[random.randint(0,len(df))])
-    st.button(label='Violento')
-    st.button(label='Violento y misógino')
-    st.button(label='No violento')
 
 
-#matrix = pd.merge(pd.DataFrame(df['label'].value_counts()),
-  #  pd.DataFrame(df['predicted_label'].value_counts()),
-  #  right_index=True,left_index=True) 
-
-
-
-
+with st.container(): 
+  st.subheader('Tweet de muestra')
+  st.write(current_tweet)
+  st.button(label='Violento',key='violent_button', on_click=vote_violent_callback)
+  st.button(label='Violento y misógino',key='mysoginist_button', on_click=vote_mysoginist_callback)
+  st.button(label='No violento',key='not_violence_button',on_click=vote_non_violent_callback)
